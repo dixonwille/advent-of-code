00bbaf10c58dfeda08d1b28c4a86f265fde5f3a7
Will Dixon <dixonwille@gmail.com>
2020-12-02 23:49:34 -0500

    performance improvements to parsers and solutions for day2

--- year2020/src/day2.rs
+++ year2020/src/day2.rs
@@ -2,8 +2,16 @@
 use std::ops::Deref;
 
 use aoc_runner_derive::{aoc, aoc_generator};
-use once_cell::sync::Lazy;
-use regex::Regex;
+use nom::{
+    bytes::complete::tag,
+    character::complete::line_ending,
+    character::complete::{alpha1, anychar, char as c, digit1},
+    combinator::all_consuming,
+    combinator::map,
+    combinator::map_res,
+    multi::separated_list1,
+    IResult,
+};
 
 #[derive(Debug, Eq, PartialEq)]
 struct Policy {
@@ -25,11 +33,14 @@ impl Policy {
 #[derive(Debug, Eq, PartialEq)]
 pub struct Password {
     policy: Policy,
-    value: String,
+    // We can use u8 for this because of our data set.
+    // real world, correct way, would be to use char or glyphs
+    // but that is less performant due to nature of how it has to be figured out
+    value: Vec<u8>, 
 }
 
 impl Password {
-    pub fn new(req1: usize, req2: usize, character: char, value: String) -> Self {
+    pub fn new(req1: usize, req2: usize, character: char, value: Vec<u8>) -> Self {
         Password {
             policy: Policy::new(req1, req2, character),
             value,
@@ -38,87 +49,94 @@ impl Password {
 }
 
 impl Deref for Password {
-    type Target = String;
+    type Target = Vec<u8>;
 
     fn deref(&self) -> &Self::Target {
         &self.value
     }
 }
 
-// #-# C: STRING
-// ##-## C: LONGERSTRING
-static PASSWORD_REGEX: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r"^(?P<req1>[[:digit:]]+)-(?P<req2>[[:digit:]]+) (?P<character>[[:alpha:]]): (?P<value>[[:alpha:]]+)$").expect("valid regular expression")
-});
-
 #[aoc_generator(day2)]
-pub fn parse_input(input: &str) -> Vec<Password> {
-    input
-        .lines()
-        .map(|l| {
-            let caps = PASSWORD_REGEX.captures(l).expect("input in proper format");
-            Password::new(
-                caps.name("req1").unwrap().as_str().parse().unwrap(),
-                caps.name("req2").unwrap().as_str().parse().unwrap(),
-                caps.name("character").unwrap().as_str().parse().unwrap(),
-                caps.name("value").unwrap().as_str().into(),
-            )
-        })
-        .collect()
+fn parse_input(input: &str) -> Vec<Password> {
+    let (_, passwords) = parse_input_nom(input).unwrap();
+    passwords
+}
+
+#[inline]
+fn take_digits(input: &str) -> IResult<&str, usize> {
+    map_res(digit1, |s: &str| s.parse())(input)
+}
+
+#[inline]
+fn take_alpha_bytes(input: &str) -> IResult<&str, Vec<u8>> {
+    map(alpha1, |s: &str| s.as_bytes().to_owned())(input)
+}
+
+#[inline]
+fn take_password(input: &str) -> IResult<&str, Password> {
+    let (input, req1) = take_digits(input)?;
+    let (input, _) = c('-')(input)?;
+    let (input, req2) = take_digits(input)?;
+    let (input, _) = c(' ')(input)?;
+    let (input, character) = anychar(input)?;
+    let (input, _) = tag(": ")(input)?;
+    let (input, value) = take_alpha_bytes(input)?;
+    Ok((input, Password::new(req1, req2, character, value)))
+}
+
+#[inline]
+fn parse_input_nom(input: &str) -> IResult<&str, Vec<Password>> {
+    all_consuming(separated_list1(line_ending, take_password))(input)
 }
 
 #[aoc(day2, part1)]
 pub fn part1(passwords: &Vec<Password>) -> usize {
-    let mut valid = 0;
-    for password in passwords {
-        let mut count = 0;
-        for c in password.chars() {
-            if c == password.policy.character {
-                count += 1;
+    passwords.iter().fold(0, |valid, password| {
+        let count = password.iter().fold(0, |count, c| {
+            if *c as char == password.policy.character {
+                return count + 1;
             }
-        }
+            count
+        });
         if count >= password.policy.req1 && count <= password.policy.req2 {
-            valid += 1;
+            return valid + 1;
         }
-    }
-    valid
+        valid
+    })
 }
 
 #[aoc(day2, part2)]
 pub fn part2(passwords: &Vec<Password>) -> usize {
-    let mut valid = 0;
-    for password in passwords {
-        let characters = password.chars().collect::<Vec<char>>();
-        let v1 = characters.get(password.policy.req1-1);
-        let v2 = characters.get(password.policy.req2-1);
+    passwords.iter().fold(0, |valid, password| {
+        let v1 = password.get(password.policy.req1 - 1);
+        let v2 = password.get(password.policy.req2 - 1);
         match (v1, v2) {
-            (None, None) => continue,
-            (Some(r1), Some(r2))=> {
-                if r1 == &password.policy.character && r2 == &password.policy.character {
-                    continue;
-                }else if r1 == &password.policy.character || r2 == &password.policy.character {
-                    valid += 1;
-                }else {
-                    continue;
-                }
-            },
-            (Some(r1), None) =>{
-                if r1 == &password.policy.character {
-                    valid += 1;
+            (None, None) => valid,
+            (Some(r1), Some(r2)) => {
+                if *r1 as char == password.policy.character && *r2 as char == password.policy.character {
+                    valid // don't increment as this is invalid
+                } else if *r1 as char == password.policy.character || *r2 as char == password.policy.character {
+                    valid + 1
                 } else {
-                    continue;
+                    valid // don't increment as this is invalid
                 }
-            },
+            }
+            (Some(r1), None) => {
+                if *r1 as char == password.policy.character {
+                    valid + 1
+                } else {
+                    valid // don't increment as this is invalid
+                }
+            }
             (None, Some(r2)) => {
-                if r2 == &password.policy.character {
-                    valid += 1;
+                if *r2 as char == password.policy.character {
+                    valid + 1
                 } else {
-                    continue;
+                    valid // don't increment as this is invalid
                 }
             }
         }
-    }
-    valid
+    })
 }
 
 #[allow(dead_code, unused_imports)]
@@ -135,9 +153,9 @@ mod test {
         assert_eq!(
             parse_input(PASSWORDS),
             vec![
-                Password::new(1, 3, 'a', "abcde".to_string()),
-                Password::new(1, 3, 'b', "cdefg".to_string()),
-                Password::new(2, 9, 'c', "ccccccccc".to_string()),
+                Password::new(1, 3, 'a', "abcde".as_bytes().to_owned()),
+                Password::new(1, 3, 'b', "cdefg".as_bytes().to_owned()),
+                Password::new(2, 9, 'c', "ccccccccc".as_bytes().to_owned()),
             ]
         );
     }
