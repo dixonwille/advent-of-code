059cd6dd031690582f70d0ed0d9749a83f802a66
Will Dixon <dixonwille@gmail.com>
2020-12-05 00:14:29 -0500

    day4 solved

--- year2020/src/day4.rs
+++ year2020/src/day4.rs
@@ -52,6 +52,97 @@ struct Passport {
     country_id: Option<usize>,
 }
 
+impl Passport {
+    fn is_valid(&self) -> bool {
+        // Must have all these fields
+        if self.birth_year.is_none()
+            || self.issue_year.is_none()
+            || self.expiration_year.is_none()
+            || self.height.is_none()
+            || self.hair_color.is_none()
+            || self.eye_color.is_none()
+            || self.passport_id.is_none()
+        {
+            return false;
+        }
+
+        let birth_year = self.birth_year.unwrap();
+        if birth_year < 1920 || birth_year > 2002 {
+            return false;
+        }
+
+        let issue_year = self.issue_year.unwrap();
+        if issue_year < 2010 || issue_year > 2020 {
+            return false;
+        }
+
+        let expiration_year = self.expiration_year.unwrap();
+        if expiration_year < 2020 || expiration_year > 2030 {
+            return false;
+        }
+
+        let height = self.height.as_ref().unwrap();
+        if height.ends_with("in") {
+            if let Ok(h) = height.replace("in", "").parse::<usize>() {
+                if h < 59 || h > 76 {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else if height.ends_with("cm") {
+            if let Ok(h) = height.replace("cm", "").parse::<usize>() {
+                if h < 150 || h > 193 {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        let hair_color = self.hair_color.as_ref().unwrap();
+        if hair_color.starts_with('#') {
+            let hair_color = hair_color.replace("#", "");
+            if hair_color.len() != 6 {
+                return false;
+            }
+            if u8::from_str_radix(&hair_color[0..2], 16).is_err()
+                || u8::from_str_radix(&hair_color[2..4], 16).is_err()
+                || u8::from_str_radix(&hair_color[4..6], 16).is_err()
+            {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        let eye_color = self.eye_color.as_ref().unwrap();
+        if eye_color.len() == 3 {
+            let valid_eye = matches!(
+                eye_color.as_ref(),
+                "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth"
+            );
+            if !valid_eye {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        let passport_id = self.passport_id.as_ref().unwrap();
+        if passport_id.len() == 9 {
+            if passport_id.parse::<usize>().is_err() {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        true
+    }
+}
+
 #[aoc_generator(day4)]
 fn parse_input(input: &str) -> Vec<Passport> {
     let (_, passports) = parse_input_nom(input).unwrap();
@@ -73,12 +164,13 @@ fn parse_field(input: &str) -> IResult<&str, (PassportField, &str)> {
         |key: &str| PassportField::from_str(key),
     )(input)?;
     let (input, _) = c(':')(input)?;
-    let (input, value) = take_till1(|c| c == ' ' || c == '\n')(input)?;
+    let (input, value) = take_till1(|c: char| c == ' ' || c == '\n')(input)?;
+    let (input, _) = opt(one_of(" \n"))(input)?; // grab the characters but I could be at the end of file!
     Ok((input, (key, value)))
 }
 
 fn parse_passport(input: &str) -> IResult<&str, Passport> {
-    let (input, fields) = many1(terminated(parse_field, one_of(" \n")))(input)?;
+    let (input, fields) = many1(parse_field)(input)?;
     let mut map = HashMap::new();
     for (key, value) in fields {
         map.insert(key, value);
@@ -112,7 +204,6 @@ fn parse_input_nom(input: &str) -> IResult<&str, Vec<Passport>> {
 
 #[aoc(day4, part1)]
 fn part1(passports: &[Passport]) -> usize {
-    println!("{:?}", passports.len());
     passports.iter().fold(0, |valid, passport| {
         if passport.birth_year.is_some()
             && passport.issue_year.is_some()
@@ -122,6 +213,17 @@ fn part1(passports: &[Passport]) -> usize {
             && passport.eye_color.is_some()
             && passport.passport_id.is_some()
         {
+            valid + 1
+        } else {
+            valid
+        }
+    })
+}
+
+#[aoc(day4, part2)]
+fn part2(passports: &[Passport]) -> usize {
+    passports.iter().fold(0, |valid, passport| {
+        if passport.is_valid() {
             valid + 1
         } else {
             valid
@@ -144,8 +246,34 @@ ecl:brn pid:760753108 byr:1931
 hgt:179cm
 
 hcl:#cfa07d eyr:2025 pid:166559648
-iyr:2011 ecl:brn hgt:59in
-";
+iyr:2011 ecl:brn hgt:59in";
+
+    static MORE_PASSPORTS: &str = "eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719";
 
     #[test]
     fn parsing_input() {
@@ -200,5 +328,11 @@ iyr:2011 ecl:brn hgt:59in
     fn running_part1() {
         let passports = parse_input(PASSPORTS);
         assert_eq!(part1(&passports), 2)
+    }
+
+    #[test]
+    fn running_part2() {
+        let passports = parse_input(MORE_PASSPORTS);
+        assert_eq!(part2(&passports), 4);
     }
 }
