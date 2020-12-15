f604ed446a96b95002e34fef79c9df9ac2462f17
Will Dixon <dixonwille@gmail.com>
2020-12-15 18:48:10 -0500

    day15 using folding and return value of HashMap's insert

--- year2020/src/day15.rs
+++ year2020/src/day15.rs
@@ -1,5 +1,5 @@
 /// https://adventofcode.com/2020/day/15
-use std::{collections::HashMap, fmt::Debug, hash::Hash};
+use std::{collections::HashMap, hash::Hash};
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
@@ -8,14 +8,11 @@ fn parse_input(input: &str) -> Vec<usize> {
     input.split(',').map(|i| i.parse().unwrap()).collect()
 }
 
-#[derive(Debug)]
 struct Mem<V> {
     map: HashMap<V, usize>,
     cache: Option<(V, usize)>,
 }
 
-
-// TODO do I need this "cache" layer or is there a smarter way to get what I want on insert.
 impl<V> Mem<V>
 where
     V: Copy + Eq + Hash,
@@ -67,14 +64,35 @@ fn nth_word_spoken(starting: &[usize], nth: usize) -> usize {
     query
 }
 
+fn nth_word_spoken2(starting: &[usize], nth: usize) -> usize {
+    let mut seen = starting[..starting.len() - 1]
+        .iter()
+        .enumerate()
+        .map(|(i, &s)| (s, i))
+        .collect::<HashMap<_, _>>();
+    (starting.len() - 1..nth - 1).fold(*starting.last().unwrap(), |last, i| {
+        i - seen.insert(last, i).unwrap_or(i)
+    })
+}
+
 #[aoc(day15, part1)]
 fn part1(starting: &[usize]) -> usize {
     nth_word_spoken(starting, 2020)
 }
 
+#[aoc(day15, part1, folding)]
+fn part1_folding(starting: &[usize]) -> usize {
+    nth_word_spoken2(starting, 2020)
+}
+
 #[aoc(day15, part2)]
 fn part2(starting: &[usize]) -> usize {
     nth_word_spoken(starting, 30_000_000)
+}
+
+#[aoc(day15, part2, folding)]
+fn part2_folding(starting: &[usize]) -> usize {
+    nth_word_spoken2(starting, 30_000_000)
 }
 
 #[cfg(test)]
@@ -84,20 +102,12 @@ mod test {
     static STARTING1: &str = "0,3,6";
     static STARTING2: &str = "1,3,2";
     static STARTING3: &str = "2,1,3";
-    static STARTING4: &str = "1,2,3";
-    static STARTING5: &str = "2,3,1";
-    static STARTING6: &str = "3,2,1";
-    static STARTING7: &str = "3,1,2";
 
     #[test]
     fn parsing_input() {
         assert_eq!(parse_input(STARTING1), vec![0, 3, 6]);
         assert_eq!(parse_input(STARTING2), vec![1, 3, 2]);
         assert_eq!(parse_input(STARTING3), vec![2, 1, 3]);
-        assert_eq!(parse_input(STARTING4), vec![1, 2, 3]);
-        assert_eq!(parse_input(STARTING5), vec![2, 3, 1]);
-        assert_eq!(parse_input(STARTING6), vec![3, 2, 1]);
-        assert_eq!(parse_input(STARTING7), vec![3, 1, 2]);
     }
 
     #[test]
@@ -108,32 +118,22 @@ mod test {
         assert_eq!(part1(&starting), 1);
         let starting = parse_input(STARTING3);
         assert_eq!(part1(&starting), 10);
-        let starting = parse_input(STARTING4);
-        assert_eq!(part1(&starting), 27);
-        let starting = parse_input(STARTING5);
-        assert_eq!(part1(&starting), 78);
-        let starting = parse_input(STARTING6);
-        assert_eq!(part1(&starting), 438);
-        let starting = parse_input(STARTING7);
-        assert_eq!(part1(&starting), 1836);
+
+        let starting = parse_input(STARTING1);
+        assert_eq!(part1_folding(&starting), 436);
+        let starting = parse_input(STARTING2);
+        assert_eq!(part1_folding(&starting), 1);
+        let starting = parse_input(STARTING3);
+        assert_eq!(part1_folding(&starting), 10);
     }
 
     #[test]
-    #[ignore = "takes long time to run (longer than a minute)"]
+    #[ignore = "takes long time to run for a unit test"]
     fn running_part2() {
         let starting = parse_input(STARTING1);
         assert_eq!(part2(&starting), 175594);
-        let starting = parse_input(STARTING2);
-        assert_eq!(part2(&starting), 2578);
-        let starting = parse_input(STARTING3);
-        assert_eq!(part2(&starting), 3544142);
-        let starting = parse_input(STARTING4);
-        assert_eq!(part2(&starting), 261214);
-        let starting = parse_input(STARTING5);
-        assert_eq!(part2(&starting), 6895259);
-        let starting = parse_input(STARTING6);
-        assert_eq!(part2(&starting), 18);
-        let starting = parse_input(STARTING7);
-        assert_eq!(part2(&starting), 362);
+
+        let starting = parse_input(STARTING1);
+        assert_eq!(part2_folding(&starting), 175594);
     }
 }
